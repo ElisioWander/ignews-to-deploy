a2c9c5c27a2391c78cd369b0e19e0ebdf0bb0ccc
ElisioWander <elisio741@hotmail.com>
2022-03-23 02:42:26 -0300

    styles-completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
-import styles from '../styles/home.module.scss'
+import Head from "next/head";
+import { SubscribeButton } from "../Components/SubscribeButton";
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title} >
-      Hello <span>world!</span>
-    </h1>
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer} >
+        <section className={styles.hero} >
+          <span>👏 Hey, welcome!</span>
+
+          <h1>News about the <span>React</span> world!</h1>
+
+          <p>
+            Get access to all publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   )
 }
