a8117c36ad0427f2289d26584ef7a8bff8cfff8c
ElisioWander <elisio741@hotmail.com>
2022-03-26 02:43:41 -0300

    next-auth-authentication

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -1,21 +1,34 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 
-import styles from './styles.module.scss'
+import { useSession, signIn, signOut } from "next-auth/react";
+
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-    const isUserLoggedIn = true
+    //session vai saber se o usuário está autenticado e trazer as informações 
+    //que foram estabelecidas dentro do provider no arquivo [...nextauth] na parte 
+    //de escopo 
+  const {data: session} = useSession()
 
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.signInButton} >
-            <FaGithub color="#04D361" />
-            Elisio Wander
-            <FiX  color="#737380" className={styles.closeIcon} />
-        </button>
-    ) : (
-        <button type="button" className={styles.signInButton} >
-            <FaGithub color="#EBA417" />
-            Sign In With Github
-        </button>
-    )
+  return session ? (
+    <button 
+        type="button"
+        className={styles.signInButton}
+        onClick={() => signOut()}
+    >
+      <FaGithub color="#04D361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#EBA417" />
+      Sign In With Github
+    </button>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { AppProps } from 'next/app'
+import { SessionProvider, SessionProviderProps } from 'next-auth/react'
 import { Header } from '../Components/Header/index'
 
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+
+function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
