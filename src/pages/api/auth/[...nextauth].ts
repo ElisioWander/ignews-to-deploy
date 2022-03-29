13dd72b26bb46908fcc439caf5a6b9ac9b07b199
ElisioWander <elisio741@hotmail.com>
2022-03-28 23:32:04 -0300

    create-and-check-if-user-exist-faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import GithubProvider from "next-auth/providers/github"
 
+import { Index, query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
@@ -15,5 +18,46 @@ export default NextAuth({
       }),
       // ...add more providers here
     ],
+    callbacks: {
+      async signIn({user, account, profile, credentials}) {
+        const { email } = user
+
+        //inserindo o email no banco de dado do usuário que efetuou o login
+        //os dados estão sendo inseridos na Collection que foi criada no dashboard do faunaDB
+        //Collection com o nome de users
+
+        try {
+          await fauna.query(
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                { data: { email } }
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            )
+            
+          )
+          return true
+        } catch {
+          console.log('Não deu')
+
+          return false
+        }
+      }
+    },
+    secret: process.env.JWT_SECRET
   })
   
