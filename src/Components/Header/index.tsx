bb4276204e81a3b204ae324c84fdb81e2e7da379
elisioWander <elisio741@hotmail.com>
2022-04-16 02:26:15 -0300

    mobile

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,22 +1,20 @@
-import { ActiveLink } from '../ActiveLink';
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss'
+import { ActiveLink } from "../ActiveLink";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
 export function Header() {
   return (
-    <header className={styles.headerContainer} >
-      <div className={styles.headerContent} >
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <ActiveLink href="/" activeClass={styles.active} >
+          <ActiveLink href="/" activeClass={styles.active}>
             <a>Home</a>
           </ActiveLink>
-          <ActiveLink href="/posts" activeClass={styles.active} >
+          <ActiveLink href="/posts" activeClass={styles.active}>
             <a>Posts</a>
           </ActiveLink>
-          
-          
         </nav>
 
         <SignInButton />
